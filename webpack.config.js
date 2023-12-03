6a0698ab9705901c3591e9c4447599e6e5a134e6
Vadim-Strakhov <mars1597@gmail.com>
2023-12-03 17:13:59 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  mode: 'development',
+  context: path.resolve('src'),
+  entry: {
+    index: '/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve('dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 8000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new MiniCssExtractPlugin({ filename: 'css/style.[contenthash].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['autoprefixer']],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
+  },
+};
